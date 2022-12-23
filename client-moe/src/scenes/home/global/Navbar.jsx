2e4385fc17a218570fb01e221444b3a9217f099a
mali295 <mali295@uwo.ca>
2022-12-23 13:18:37 -0500

    Added badge for cart value change

--- client-moe/src/scenes/home/global/Navbar.jsx
+++ client-moe/src/scenes/home/global/Navbar.jsx
@@ -2,11 +2,13 @@ import {useDispatch, useSelector} from 'react-redux'
 import { Badge, Box, IconButton} from "@mui/material"
 import { PersonOutline, ShoppingBagOutlined, MenuOutlined, SearchOutlined} from '@mui/icons-material'
 import { useNavigate } from 'react-router-dom';
-import { shades } from "../../theme"
+import { shades } from "../../../theme";
+import { setIsCartOpen } from '../../../state';
 
 const Navbar = () => {
     const navigate = useNavigate();
     const dispatch = useDispatch();
+    const cart = useSelector((state) => state.cart.cart);
 
     return <Box
         display="flex"
@@ -46,9 +48,27 @@ const Navbar = () => {
                 <IconButton sx={{color: "black"}}>
                     <PersonOutline />
                 </IconButton>
-                <IconButton sx={{color: "black"}}>
+                <Badge
+                    badgeContent={cart.length}
+                    color="secondary"
+                    invisible={cart.length === 0}
+                    sx={{
+                        "&.MuiBadge-badge": {
+                            right: 5,
+                            top: 5,
+                            padding: "0, 4px",
+                            height: "14px",
+                            minWidth: "13px",
+                        },
+                    }} 
+                >
+                <IconButton
+                    onClick={() => dispatch(setIsCartOpen({}))}
+                    sx={{color: "black"}}
+                >
                     <ShoppingBagOutlined />
                 </IconButton>
+                </Badge>
                 <IconButton sx={{color: "black"}}>
                     <MenuOutlined />
                 </IconButton>
