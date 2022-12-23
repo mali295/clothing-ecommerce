16d2d8f588dccbc298fcd12392d4f1c1a46d17b1
mali295 <mali295@uwo.ca>
2022-12-23 12:54:18 -0500

    Added logic for redux toolkit for cart

--- client-moe/src/state/index.js
+++ client-moe/src/state/index.js
@@ -13,12 +13,47 @@ export const cartSlice = createSlice({
         setItems: (state, action) => {
             state.items = action.payload;
         },
-        
+
+        addToCart: (state, action) => {
+            state.cart = [...state.cart, action.payload.item]
+        },
+
+        removeFromCart: (state, action) => {
+            state.cart = state.cart.filter((item) => item.id !== action.payload.id);
+        },
+
+        increaseCount: (state, action) => {
+            state.cart = state.cart.map((item) => {
+                if (item.id === action.payload.id) {
+                    item.count++;
+                }
+                return item;
+            });
+        },
+
+        decreaseCount: (state, action) => {
+            state.cart = state.cart.map((item) => {
+                if (item.id === action.payload.id && item.count > 1) {
+                    item.count--;
+                }
+                return item;
+            });
+        },
+
+        setIsCartOpen: (state) => {
+            state.isCartOpen = !state.isCartOpen;
+        }
+
     }
-})
+});
 
 export const {
-    setItems
+    setItems,
+    addToCart,
+    removeFromCart,
+    increaseCount,
+    decreaseCount,
+    setIsCartOpen,
 } = cartSlice.actions;
 
 export default cartSlice.reducer
