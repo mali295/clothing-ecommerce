936c9533a2bf9e98a5937297c351bd9765d685bc
mali295 <mali295@uwo.ca>
2022-12-23 18:29:23 -0500

    Complete individual item component

--- client-moe/src/App.js
+++ client-moe/src/App.js
@@ -26,7 +26,7 @@ function App() {
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="item/:itemID" element={<ItemDetails />} />
+          <Route path="item/:itemId" element={<ItemDetails />} />
           <Route path="checkout" element={<Checkout />} />
           <Route path="checkout/success" element={<Confirmation />} />
         </Routes>
--- client-moe/src/components/Item.jsx
+++ client-moe/src/components/Item.jsx
@@ -40,8 +40,59 @@ const Item = ({ item, width}) => {
                     height="400px"
                     src={`http:localhost:3000${url}`}
                     onClick={() => navigate(`/item/${item.id}`)}
+                    style={{ cursor: 'pointer'}}
                 />
+                <Box
+                    display={isHovered ? "blocked" : 'none'}
+                    position= "absolute"
+                    bottom="10%"
+                    left="0"
+                    width="100%"
+                    padding= "0 5%"
+                >
+                    <Box display="flex" justifyContent="space-between">
 
+                        <Box 
+                        display="flex" 
+                        alignItems="center" 
+                        backgroundColor={shades.neutral[100]}
+                        borderRadius="3px"
+                        >
+                            <IconButton
+                                onClick={() => setCount(Math.max(count, -1,1))}
+                            >
+                                <RemoveIcon />
+                            </IconButton>
+                            <Typography color={shades.primary[300]}>{count}</Typography>
+                            <IconButton
+                                onClick={() => setCount(count + 1)}
+                            >
+                                <AddIcon />
+                            </IconButton>
+
+
+                        </Box>
+
+                        <Button
+                            onClick={() => {
+                                dispatch(addToCart({ item: {...item, count}}))
+                            }}
+                            sx={{backgroundColor: shades.primary[300], color: "white"}}
+                        >
+                            Add to Cart
+                        </Button>
+                    </Box>
+                </Box>
+            </Box>
+
+            <Box mt="3px">
+                <Typography variant="subtitle2" color={neutral.dark}>
+                    {category
+                        .replace(/(A-Z)/g, " $1")
+                        .replace(/^./, (str) => str.toUpperCase())}
+                </Typography>
+                <Typography>{name}</Typography>
+                <Typography fontWeight="bold">${price}</Typography>
             </Box>
         </Box>
     )
