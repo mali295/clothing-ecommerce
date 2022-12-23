c22c1a9d6a3491bb371f4e9f4592a72c538cf2cc
mali295 <mali295@uwo.ca>
2022-12-23 12:43:02 -0500

    Initialized Redux ToolKit

---
+++ client-moe/src/state/index.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+    isCartOpen: false,
+    cart: [],
+    items: [],
+}
+
+export const cartSlice = createSlice({
+    name: "cart",
+    initialState,
+    reducers: {
+        setItems: (state, action) => {
+            state.items = action.payload;
+        },
+        
+    }
+})
+
+export const {
+    setItems
+} = cartSlice.actions;
+
+export default cartSlice.reducer
