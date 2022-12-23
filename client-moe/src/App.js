8a88a157aaa76bad5d7aa540539922e785df4f99
mali295 <mali295@uwo.ca>
2022-12-23 11:02:51 -0500

    Completed Theme Setup

---
+++ client-moe/src/App.js
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useLocation
+} from 'react-router-dom'
+
+
+
+function App() {
+  return (
+    <div className="app">
+
+    </div>
+  );
+}
+
+export default App;
