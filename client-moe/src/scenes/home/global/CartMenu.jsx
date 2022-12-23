0bd8d7f538c01d4988f76e7e661fa5d989d812fb
mali295 <mali295@uwo.ca>
2022-12-23 14:44:18 -0500

    Create modal and overlay for cart

---
+++ client-moe/src/scenes/home/global/CartMenu.jsx
@@ -0,0 +1,66 @@
+import { Box, Button, Divider, IconButton, Typography } from "@mui/material"
+import { useSelector, useDispatch } from "react-redux"
+import CloseIcon from "@mui/icons-material/Close"
+import AddIcon from "@mui/icons-material/Add"
+import RemoveIcon from "@mui/icons-material/Remove"
+import styled from "@emotion/styled"
+import { shades } from "../../../theme"
+import { decreaseCount, increaseCount, removeFromCart, setIsCartOpen } from "../../../state"
+import { useNavigate } from "react-router-dom"
+
+const FlexBox = styled(Box)`
+    display: flex;
+    justify-content: space-between
+    align-items: center;
+`;
+
+const CartMenu = () => {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const cart = useSelector((state) => state.cart.cart);
+    const setIsCartOpen = useSelector((state) => state.cart.setIsCartOpen);
+
+    const totalPrice = cart.reduce((total, item) => {
+        return total + item.count * item.attributes.price;
+    }, 0)
+
+    return (
+        <Box
+            display={setIsCartOpen ? "block": 'none'}
+            backgroundColor="rgba(0,0,0,0.4)"
+            position="fixed"
+            zIndex={10}
+            width="100%"
+            height="100%"
+            left="0"
+            top="0"
+            overflow="auto"
+        >
+
+            <Box
+                position="fixed"
+                right="0"
+                bottom="0"
+                width="max(400px, 30%)"
+                height="100%"
+                backgroundColor="white"
+            >
+                <Box padding="30p" overflow="auto" height="100%">
+                    <FlexBox mb="15px">
+                        <Typography variant="h3">SHOPPING BAG ({cart.length})</Typography>
+                        <IconButton onClick={() => dispatch(setIsCartOpen({}))}>
+                            <CloseIcon />
+                        </IconButton>
+
+                    </FlexBox>
+
+                </Box>
+
+            </Box>
+
+        </Box>
+
+    )
+}
+
+export default CartMenu;
