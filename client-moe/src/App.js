fbea8f2a7bbe89077aa7b4142629c2cbe28b37a9
mali295 <mali295@uwo.ca>
2022-12-23 12:32:33 -0500

    Implemented Navbar

--- client-moe/src/App.js
+++ client-moe/src/App.js
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
 import Checkout from "./scenes/home/checkout/Checkout";
 import Confirmation from "./scenes/home/checkout/Confirmation";
+import Navbar from "./scenes/home/global/Navbar";
 import Home from "./scenes/home/Home"
 import ItemDetails from "./scenes/home/itemDetails/ItemDetails";
 
@@ -20,6 +21,7 @@ function App() {
   return (
     <div className="app">
       <BrowserRouter>
+        <Navbar />
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
